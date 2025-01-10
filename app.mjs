7db5c7b0220762c4da0d186800e17948978f2b2a
1gourab0 <159064735+1gourab0@users.noreply.github.com>
2025-01-10 22:35:23 +0530

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,103 +1,63 @@
-import express from "express";
-import cache from "./cache.mjs";
-import { v4 as uuidv4 } from "uuid";
+const express = require("express");
+const morgan = require("morgan");
+const chalk = require("chalk");
+const responseTime = require("response-time");
+const rate_limiter = require("./limiter/rate_limiter");
+const speed_limiter = require("./limiter/speed_limiter");
+const user_routes = require("./routes/user_routes");
+morgan.token("status", (req, res) => {
+  const status = res.statusCode;
+  const color =
+    status >= 500
+      ? "red"
+      : status >= 400
+      ? "yellow"
+      : status >= 300
+      ? "cyan"
+      : status >= 200
+      ? "green"
+      : "white";
+  return chalk[color](status);
+});
+
+morgan.token("method", (req, res) => {
+  return chalk.blue(req.method);
+});
+
+morgan.token("url", (req, res) => {
+  return chalk.magenta(req.url);
+});
+
+morgan.token("date", (req, res) => {
+  return chalk.gray(new Date().toISOString());
+});
+
+morgan.token("response-time", (req, res) => {
+  let responseTime = parseFloat(res.getHeader("X-Response-Time"));
+
+  if (responseTime > 1000) {
+    responseTime = `${(responseTime / 1000).toFixed(2)} s`;
+  } else {
+    responseTime = `${responseTime} ms`;
+  }
+  return chalk.yellow(responseTime);
+});
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// add a book - request body should contain a title, status and an author
-app.post("/reading-list/books", (req, res) => {
-  const { title, author, status } = req.body;
-  const uuid = uuidv4();
-  if (!(status === "read" || status === "to_read" || status === "reading")) {
-    return res.status(400).json({
-      error: "Status is invalid. Accepted statuses: read | to_read | reading",
-    });
-  }
-  if (!title || !author || !status) {
-    return res.status(400).json({ error: "Title, Status or Author is empty" });
-  }
-  const value = { uuid, title, author, status };
-  cache.set(uuid, value, 86400);
-  return res.status(201).json({ uuid, title, author });
+app.set("trust proxy", 1); // Enable trust proxy
+
+app.use(responseTime()); // to set the X-Response-Time header
+app.use(morgan(":date :method :url :status :response-time"));
+app.use(speed_limiter);
+app.use(rate_limiter);
+app.use(express.json()); // middleware for parsing JSON bodies
+
+// Routing to microservices
+app.use("/users", user_routes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "gateway says ok!" });
 });
 
-// update status of a book by uuid
-app.put("/reading-list/books/:uuid", (req, res) => {
-  const uuid = req.params.uuid;
-  const { status } = req.body;
-  if (!uuid || typeof uuid !== "string") {
-    return res.status(400).json({ error: "missing or invalid UUID" });
-  }
-  if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
-  }
-  if (!(status === "read" || status === "to_read" || status === "reading")) {
-    return res.status(400).json({
-      error: "Status is invalid. Accepted statuses: read | to_read | reading",
-    });
-  }
-  const value = cache.get(uuid);
-  value.status = status;
-  cache.set(uuid, value);
-  return res.json({ uuid, status });
-});
-
-// get the list of books
-app.get("/reading-list/books", (_, res) => {
-  const keys = cache.keys();
-  const allData = {};
-  for (const key of keys) {
-    allData[key] = cache.get(key);
-  }
-  return res.json(allData);
-});
-
-// get a book by uuid
-app.get("/reading-list/books/:uuid", (req, res) => {
-  const uuid = req.params.uuid;
-  if (!uuid || typeof uuid !== "string") {
-    return res.status(400).json({ error: "missing or invalid UUID" });
-  }
-  if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
-  }
-  const value = cache.get(uuid);
-  return res.json(value);
-});
-
-// delete a book by uuid
-app.delete("/reading-list/books/:uuid", (req, res) => {
-  const uuid = req.params.uuid;
-  if (!uuid || typeof uuid !== "string") {
-    return res.status(400).json({ error: "missing or invalid UUID" });
-  }
-  if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
-  }
-  cache.del(uuid);
-  return res.json({ uuid });
-});
-
-// health check
-app.get("/reading-list/healthz", (_, res) => {
-  return res.sendStatus(200);
-});
-
-app.use((err, _req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-  console.error(err);
-  res.status(500);
-  res.json({ error: err.message });
-});
-
-app.use("*", (_, res) => {
-  return res
-    .status(404)
-    .json({ error: "the requested resource does not exist on this server" });
-});
-
-export default app;
+module.exports = app;
